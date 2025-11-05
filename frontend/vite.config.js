3c9d0a3e7320eb233c79daa4284a9d346765eeb4
Allen Alex Thomas <140880531+Alex0-7@users.noreply.github.com>
2025-11-05 21:12:14 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/car_rental",
+  base:"/car_rental/",
   server: {
     port: 3000,
   },
